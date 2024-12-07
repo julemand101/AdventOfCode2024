90250da07d03fdb07d3ff08653fbda857b59e962
Jacob Bang <julemand101@archlinux.dk>
2024-12-07 22:58:12 +0100

    Day06a: Solved
    Need some mental prepare for second part...

---
+++ lib/day06.dart
@@ -0,0 +1,99 @@
+// --- Day 6: Guard Gallivant ---
+// https://adventofcode.com/2024/day/6
+
+import 'dart:typed_data';
+
+const guardChar = 94; // ^
+const emptyChar = 46; // .
+const obstructionChar = 35; // #
+
+const movements = [
+  // Up
+  Point(0, -1),
+  // Right
+  Point(1, 0),
+  // Down
+  Point(0, 1),
+  // Left
+  Point(-1, 0),
+];
+
+int solveA(List<String> input) {
+  final grid = Grid(input.first.length, input.length)..setFromInput(input);
+  final distinctPositions = <Point>{};
+
+  var guardPosition = grid.guardStartingPoint;
+  var direction = 0;
+
+  while (true) {
+    distinctPositions.add(guardPosition);
+
+    final nextGuardPosition = guardPosition + movements[direction];
+    final objectOnNewPosition = grid.getByPoint(nextGuardPosition);
+
+    if (objectOnNewPosition == null) {
+      // Hit outside of grid which means we are done
+      break;
+    } else if (objectOnNewPosition == obstructionChar) {
+      // Hit obstacle so rotate
+      direction = (direction + 1) % movements.length;
+    } else {
+      // Move to free space
+      guardPosition = nextGuardPosition;
+    }
+  }
+
+  return distinctPositions.length;
+}
+
+int solveB(List<String> input) {
+  return 0;
+}
+
+extension type const Point._(({int x, int y}) _point) {
+  const Point(int x, int y) : this._((x: x, y: y));
+
+  int get x => _point.x;
+  int get y => _point.y;
+
+  Point operator +(Point other) => Point(
+        x + other.x,
+        y + other.y,
+      );
+
+  Point operator -(Point other) => Point(
+        x - other.x,
+        y - other.y,
+      );
+}
+
+class Grid {
+  final int length, height;
+  final Uint8List _list;
+  late final Point guardStartingPoint;
+
+  Grid(this.length, this.height) : _list = Uint8List(length * height);
+
+  int? getByPoint(Point p) => get(p.x, p.y);
+  int? get(int x, int y) => (x >= 0 && x < length && y >= 0 && y < height)
+      ? _list[_getPos(x, y)]
+      : null;
+
+  void setByPoint(Point p, int value) => set(p.x, p.y, value);
+  void set(int x, int y, int value) => _list[_getPos(x, y)] = value;
+
+  int _getPos(int x, int y) => x + (y * length);
+
+  void setFromInput(List<String> input) {
+    for (final (y, line) in input.indexed) {
+      for (var x = 0; x < line.length; x++) {
+        final codeUnit = line.codeUnitAt(x);
+        set(x, y, codeUnit);
+
+        if (codeUnit == guardChar) {
+          guardStartingPoint = Point(x, y);
+        }
+      }
+    }
+  }
+}
---
+++ test/day06_test.dart
@@ -0,0 +1,56 @@
+// --- Day 6: Guard Gallivant ---
+// https://adventofcode.com/2024/day/6
+
+import 'dart:io';
+import 'package:advent_of_code_2024/day06.dart';
+import 'package:advent_of_code_2024/util.dart';
+import 'package:test/test.dart';
+
+final input = File('test/data/day06.txt').readAsLinesSync();
+
+void main() {
+  group('Part One', () {
+    test('Example 1', () {
+      expect(
+          solveA(r'''
+....#.....
+.........#
+..........
+..#.......
+.......#..
+..........
+.#..^.....
+........#.
+#.........
+......#...
+'''
+              .toLinesList()),
+          equals(41));
+    });
+    test('Solution', () {
+      expect(solveA(input), equals(4696));
+    });
+  });
+  group('Part Two', () {
+    test('Example 1', () {
+      expect(
+          solveB(r'''
+....#.....
+.........#
+..........
+..#.......
+.......#..
+..........
+.#..^.....
+........#.
+#.........
+......#...
+'''
+              .toLinesList()),
+          equals(6));
+    });
+    test('Solution', () {
+      expect(solveB(input), equals(-1));
+    });
+  }, skip: true);
+}
