e0ecce3c106179ccec011f6e0d100947559c1a15
Jacob Bang <julemand101@archlinux.dk>
2024-12-07 16:29:28 +0100

    Day05: Solved

---
+++ lib/day05.dart
@@ -0,0 +1,55 @@
+// --- Day 5: Print Queue ---
+// https://adventofcode.com/2024/day/5
+
+import 'package:collection/collection.dart';
+
+int solveA(Iterable<String> input) => getPageLists(input, getSorted: true)
+    .map((o) => o.pageList[o.pageList.length ~/ 2])
+    .sum;
+
+int solveB(Iterable<String> input) => getPageLists(input, getSorted: false)
+    .map((o) => o.pageList..sort((p1, p2) => comparePages(o.pageRules, p1, p2)))
+    .map((pageList) => pageList[pageList.length ~/ 2])
+    .sum;
+
+Iterable<({List<int> pageList, Map<int, Set<int>> pageRules})> getPageLists(
+  Iterable<String> input, {
+  required bool getSorted,
+}) sync* {
+  final pageOrderingRules = <int, Set<int>>{};
+
+  var parsePageOrderingRules = true;
+  for (final line in input) {
+    if (line == '') {
+      parsePageOrderingRules = false;
+      continue;
+    }
+
+    if (parsePageOrderingRules) {
+      final [partA, partB] = line.split('|');
+
+      pageOrderingRules.update(
+        int.parse(partA),
+        (set) => set..add(int.parse(partB)),
+        ifAbsent: () => {int.parse(partB)},
+      );
+    } else {
+      if ([...line.split(',').map(int.parse)] case final pageList
+          when pageList.isSorted((page1, page2) =>
+                  comparePages(pageOrderingRules, page1, page2)) ==
+              getSorted) {
+        yield (pageList: pageList, pageRules: pageOrderingRules);
+      }
+    }
+  }
+}
+
+int comparePages(Map<int, Set<int>> pageOrderingRules, int page1, int page2) {
+  if (pageOrderingRules[page1] case final rules? when rules.contains(page2)) {
+    return -1;
+  }
+  if (pageOrderingRules[page2] case final rules? when rules.contains(page1)) {
+    return 1;
+  }
+  return 0;
+}
---
+++ test/day05_test.dart
@@ -0,0 +1,92 @@
+// --- Day 5: Print Queue ---
+// https://adventofcode.com/2024/day/5
+
+import 'dart:io';
+import 'package:advent_of_code_2024/day05.dart';
+import 'package:advent_of_code_2024/util.dart';
+import 'package:test/test.dart';
+
+final input = File('test/data/day05.txt').readAsLinesSync();
+
+void main() {
+  group('Part One', () {
+    test('Example 1', () {
+      expect(
+          solveA(r'''
+47|53
+97|13
+97|61
+97|47
+75|29
+61|13
+75|53
+29|13
+97|29
+53|29
+61|53
+97|53
+61|29
+47|13
+75|47
+97|75
+47|61
+75|61
+47|29
+75|13
+53|13
+
+75,47,61,53,29
+97,61,53,29,13
+75,29,13
+75,97,47,61,53
+61,13,29
+97,13,75,29,47
+'''
+              .asLines),
+          equals(143));
+    });
+    test('Solution', () {
+      expect(solveA(input), equals(6951));
+    });
+  });
+  group('Part Two', () {
+    test('Example 1', () {
+      expect(
+          solveB(r'''
+47|53
+97|13
+97|61
+97|47
+75|29
+61|13
+75|53
+29|13
+97|29
+53|29
+61|53
+97|53
+61|29
+47|13
+75|47
+97|75
+47|61
+75|61
+47|29
+75|13
+53|13
+
+75,47,61,53,29
+97,61,53,29,13
+75,29,13
+75,97,47,61,53
+61,13,29
+97,13,75,29,47
+'''
+              .asLines),
+          equals(123));
+    });
+    test('Solution', () {
+      expect(solveB(input), equals(4121));
+    });
+  });
+}
